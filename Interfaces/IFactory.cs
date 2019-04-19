be6be1dbace651144b2a80f25db9d16c18a4c259
Shuaib Sajid <38845470+Shu52@users.noreply.github.com>
2019-04-19 11:25:09 -0500

    Added comments

--- Interfaces/IFactory.cs
+++ Interfaces/IFactory.cs
@@ -1,9 +1,15 @@
 using System.Collections.Generic;
 using PennyPacker.Models.Workers;
-
+/* each of these instances of GenericList<T>, every occurrence of T in the class is substituted at run time with the type argument.
+By means of this substitution, we have created three separate type-safe and efficient objects using a single class definition.
+*/
+//Declare namespace
 namespace PennyPacker.Interfaces {
+    // interface name and generic type
     public interface IFactory<T> {
+        //list og generic type
         List<T> employees { get; set; }
+        //generic taco
         void HireEmployee (T taco);
     }
 }
